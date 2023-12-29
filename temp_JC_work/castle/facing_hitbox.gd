985b9459e7a174154376b60bb3e4e4c229bc3483
Juan Carlos Oliveros <jcoliveros@mac.com>
2023-12-29 11:25:55 -0800

    remove subtree: temp_JC_work

--- temp_JC_work/castle/facing_hitbox.gd
+++
@@ -1,4 +0,0 @@
-extends CollisionShape2D
-
-@export var facing_left_position : Vector2
-@export var facing_right_position : Vector2
--- temp_JC_work/sideScroller_STATES/signal_bus.gd
+++
@@ -1,5 +0,0 @@
-extends Node
-
-signal on_health_changed(node : Node, amountChanged : int)
-
-#this will be a global variable
